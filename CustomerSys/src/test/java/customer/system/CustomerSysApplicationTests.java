0204b51de34f51839c7e713ed011f750321474eb
Hieu Phan <Hieu Phan@DESKTOP-DPELKIJ.lan>
2019-06-18 22:58:46 +0700

    spring-new

---
+++ CustomerSys/src/test/java/customer/system/CustomerSysApplicationTests.java
@@ -0,0 +1,16 @@
+package customer.system;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CustomerSysApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
